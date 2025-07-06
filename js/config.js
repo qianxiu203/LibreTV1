a4d858e36f12c3209a8294e2a7d0d82fcb3250c1
qianxiu203 <64684893+qianxiu203@users.noreply.github.com>
2025-07-06 15:21:51 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -168,6 +168,11 @@ const API_SITES = {
     //     name: '玉兔资源',
     //     adult: true
     // },
+    ckzy: {
+         api: 'https://xingba111.com/api.php/provide/vod',
+         name: '杏吧资源',
+         adult: true
+     },
 
     // 下面是资源失效率高的API源，不建议使用
     // subo: {
