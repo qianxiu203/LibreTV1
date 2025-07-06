5710ccdbb1f052f6a5f5baae43e20e146cfccf7c
qianxiu203 <64684893+qianxiu203@users.noreply.github.com>
2025-07-06 15:16:12 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -110,9 +110,13 @@ const API_SITES = {
         api: 'https://ikunzyapi.com/api.php/provide/vod',
         name: 'iKun资源'
     },
-    testSource: {
+    maotai: {
         api: 'https://caiji.maotaizy.cc/api.php/provide/vod',
-        name: 'maotai',
+        name: '茅台资源'
+    },
+    testSource: {
+        api: 'https://www.example.com/api.php/provide/vod',
+        name: '空内容测试源',
         adult: true
     },
     // 下面是一些成人内容的API源，默认隐藏，使用本项目浏览黄色内容违背项目初衷
